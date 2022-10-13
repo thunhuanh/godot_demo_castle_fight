1fa596c3f265806ab7637b956781ce87e2ffb332
Nhữ Anh Thư <bovachd@gmail.com>
2022-10-13 14:17:29 +0700

    update cloud shader and some graphic

--- Scripts/BuildingShop.gd
+++ Scripts/BuildingShop.gd
@@ -1,16 +1,22 @@
 extends NinePatchRect
 
-
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 
+export var camera : PackedScene
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	# lay thong tin từu deck 
+	# hien thi
 	pass # Replace with function body.
 	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
-#	pass
+#	if camera == null:
+#		return
+#	self.set_position(camera.get_position())
+
